307916966e3161696120de6e06053b1716a68f0e
srikanthnani <srikanthnani1202@gmail.com>
2023-09-30 22:01:10 +0530

    :bento: assets and home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useEffect, useState } from "react";
+import { Container, Video } from "./styles";
+// Importing video's from assets
+import Video1 from "../assets/video-1.mp4";
+import Video2 from "../assets/video-2.mp4";
+import Video3 from "../assets/video-3.mp4";
 
 type Props = {};
 
+const videos = [Video1, Video2, Video3];
+
 const App = ({}: Props) => {
-  return <Container>Hello world</Container>;
+  return (
+    <Container>
+      {/* to Select a random video from the 'videos' array */}
+      <Video autoPlay muted loop>
+        <source
+          src={`${videos[Math.floor(Math.random() * videos.length)]}`}
+          type="video/mp4"
+        />
+      </Video>
+    </Container>
+  );
 };
 
 export default App;
--- src/app/styles.tsx
+++ src/app/styles.tsx
@@ -1,3 +1,17 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: -1;
+`;
+
+export const Video = styled.video`
+  min-width: 100%;
+  min-height: 100%;
+  object-fit: cover;
+`;
