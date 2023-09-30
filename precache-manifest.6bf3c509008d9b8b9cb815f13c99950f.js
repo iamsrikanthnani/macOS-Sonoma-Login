f04212d37422e6994de341e0eed76cc22b07a5eb
srikanthnani <srikanthnani1202@gmail.com>
2023-09-30 19:52:47 +0530

    Initial deploy :rocket:

---
+++ precache-manifest.6bf3c509008d9b8b9cb815f13c99950f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d628be33dc911279c3",
+    "url": "/macOS-Sonoma-Login/static/css/main.b732484b.chunk.css"
+  },
+  {
+    "revision": "74d628be33dc911279c3",
+    "url": "/macOS-Sonoma-Login/static/js/main.74d628be.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/macOS-Sonoma-Login/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/macOS-Sonoma-Login/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "4ab508e54be8012bee37",
+    "url": "/macOS-Sonoma-Login/static/js/runtime~main.4ab508e5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/macOS-Sonoma-Login/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "19e7b5e76c0b4b9019c5807d370c694c",
+    "url": "/macOS-Sonoma-Login/index.html"
+  }
+];
