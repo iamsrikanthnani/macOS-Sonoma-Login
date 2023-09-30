72dc04154868bd373efa741ca2cef687cc60d4a5
srikanthnani <srikanthnani1202@gmail.com>
2023-09-30 20:23:03 +0530

    configs and setup :sparkles:

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styles";
+
+type Props = {};
+
+const App = ({}: Props) => {
+  return <Container>Hello world</Container>;
+};
+
+export default App;
